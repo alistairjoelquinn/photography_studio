9c8b95f89f83974c0d77fc780cf2735e6114ba94
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-08-27 20:15:24 +0200

    more sandbox experiments for choosing best formatting

---
+++ pages/more.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="button-cont">
+    <a href="#" class="button is-white">White</a>
+    <a href="#" class="button is-light">Light</a>
+    <a href="#" class="button is-dark">Dark</a>
+    <a href="#" class="button is-info">Info</a>
+    <a href="#" class="button is-info is-outlined">Info</a>
+    <a href="#" class="button is-success is-outlined">Success</a>
+    <a href="#" class="button is-info is-inverted">Info</a>
+    <a href="#" class="button is-success is-inverted">Success</a>
+    <a href="#" class="button is-hovered is-small">Hovered</a>
+    <a href="#" class="button is-focused is-medium">Focused</a>
+    <a href="#" class="button is-active is-large">Active</a>
+    <a href="#" class="button is-loading">Loading</a>
+  </div>
+</template>
+
+<style scoped>
+.button-cont {
+  padding: 30px;
+}
+</style>
--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
-    <div class="box marginer">
+    <div style="height: 30px;"></div>
+    <div class="box banner">
       <h1 class="title">This is a box</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ipsa, debitis praesentium, quod vitae non optio harum libero corrupti quidem enim beatae mollitia, quas molestias? Facere quasi ducimus natus rem?</p>
     </div>
+    <nuxt-link to="/more" class="white">More</nuxt-link>
   </div>
 </template>
 
 <style scoped>
-.marginer {
-  margin: 30px;
+.white {
+  color: antiquewhite;
 }
 </style>
