a7fab5ee9d7c7e05f8dc50745700e62477203eec
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-08-27 19:54:41 +0200

    sandbox added, favicon and title updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,18 +2,19 @@ export default {
     mode: 'universal',
     target: 'server',
     head: {
-        title: process.env.npm_package_name || '',
+        title: `Teniya's Photo Studio`,
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
             { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'icon', type: 'image/x-icon', href: '/camera.ico' },
             { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' }
         ]
     },
     css: [
+        '~static/main.css'
     ],
     plugins: [
     ],
---
+++ pages/sandbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="box marginer">
+      <h1 class="title">This is a box</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ipsa, debitis praesentium, quod vitae non optio harum libero corrupti quidem enim beatae mollitia, quas molestias? Facere quasi ducimus natus rem?</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.marginer {
+  margin: 30px;
+}
+</style>
