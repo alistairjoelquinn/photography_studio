16cd84feaec00ed094443c8d438419807e3fe55f
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-08-28 16:58:02 +0200

    card moved to component, color scheme moved to main.scss

---
+++ components/PhotoCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="column">
+    <div class="card">
+      <div class="card-content">
+        <h2 class="title">Photo Title</h2>
+        <h3 class="subtitle">Subtitle</h3>
+      </div>
+      <footer class="card-footer">
+        <div class="card-footer-item">
+          <a href="#" class="button is-success">
+            <i class="fa fa-thumbs-o-up"></i>
+          </a>
+        </div>
+        <div class="card-footer-item">
+          <a href="#" class="button tomato">
+            <i class="fa fa-thumbs-o-down"></i>
+          </a>
+        </div>
+        <div class="card-footer-item">
+          <a href="#" class="button is-info">
+            <i class="fa fa-retweet"></i>
+          </a>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section class="hero heading">
+    <section class="hero tomato">
       <div class="hero-body">
         <div class="container">
           <h1 class="title is-1 header-text">Teniya Salazar</h1>
@@ -9,91 +9,24 @@
       </div>
     </section>
     <div class="columns main-content">
-      <div class="column">
-        <div class="card">
-          <div class="card-content">
-            <h2 class="title">Title</h2>
-            <h3 class="subtitle">Subtitle</h3>
-          </div>
-          <footer class="card-footer">
-            <div class="card-footer-item">
-              <a href="#" class="button is-success">
-                <i class="fa fa-thumbs-o-up"></i>
-              </a>
-            </div>
-            <div class="card-footer-item">
-              <a href="#" class="button is-danger">
-                <i class="fa fa-thumbs-o-down"></i>
-              </a>
-            </div>
-            <div class="card-footer-item">
-              <a href="#" class="button is-info">
-                <i class="fa fa-retweet"></i>
-              </a>
-            </div>
-          </footer>
-        </div>
-      </div>
-      <div class="column">
-        <div class="card">
-          <div class="card-content">
-            <h2 class="title">Title</h2>
-            <h3 class="subtitle">Subtitle</h3>
-          </div>
-          <footer class="card-footer">
-            <div class="card-footer-item">
-              <a href="#" class="button is-success">
-                <i class="fa fa-thumbs-o-up"></i>
-              </a>
-            </div>
-            <div class="card-footer-item">
-              <a href="#" class="button is-danger">
-                <i class="fa fa-thumbs-o-down"></i>
-              </a>
-            </div>
-            <div class="card-footer-item">
-              <a href="#" class="button is-info">
-                <i class="fa fa-retweet"></i>
-              </a>
-            </div>
-          </footer>
-        </div>
-      </div>
-      <div class="column">
-        <div class="card">
-          <div class="card-content">
-            <h2 class="title">Title</h2>
-            <h3 class="subtitle">Subtitle</h3>
-          </div>
-          <footer class="card-footer">
-            <div class="card-footer-item">
-              <a href="#" class="button is-success">
-                <i class="fa fa-thumbs-o-up"></i>
-              </a>
-            </div>
-            <div class="card-footer-item">
-              <a href="#" class="button is-danger">
-                <i class="fa fa-thumbs-o-down"></i>
-              </a>
-            </div>
-            <div class="card-footer-item">
-              <a href="#" class="button is-info">
-                <i class="fa fa-retweet"></i>
-              </a>
-            </div>
-          </footer>
-        </div>
-      </div>
+      <PhotoCard />
+      <PhotoCard />
+      <PhotoCard />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import PhotoCard from "~/components/PhotoCard";
+
+export default {
+  components: {
+    PhotoCard,
+  },
+};
 </script>
 
 <style lang="scss" >
-@import "~assets/scss/main.scss";
 section {
   margin-bottom: 20px;
 }
@@ -101,8 +34,8 @@ section {
   width: 90vw;
   margin: 0 auto;
 }
-.heading {
-  background-color: tomato;
+.tomato {
+  background-color: $main-color;
 }
 .header-text {
   color: antiquewhite;
